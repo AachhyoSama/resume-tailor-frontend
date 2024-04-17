fd51f9b0c77ce17502b7cafa9383fef5dcfb7155
Anish Gurung <gurunganish1430@gmail.com>
2024-04-17 11:09:45 -0700

    New version

--- src/app/pages/e-commerce/e-commerce.component.ts
+++ src/app/pages/e-commerce/e-commerce.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ngx-ecommerce',
   templateUrl: './e-commerce.component.html',
 })
-export class ECommerceComponent {
+export class ECommerceComponent implements OnInit {
+
+  queryParams: any;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    // Get the query parameters from the URL
+    this.route.queryParams.subscribe(params => {
+      this.queryParams = params;
+    });
+  }
 }
